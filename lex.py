8cca657853b6304a473d9fe60e2dd93d61a14dc5
Evan Ellis <portalevan@gmail.com>
2024-01-27 21:13:30 -0800

    init commit

---
+++ lex.py
@@ -0,0 +1,31 @@
+class Lexer:
+    def __init__(self, source):
+        self.source = source + '\n'
+        self.curChar = ''
+        self.curPos = -1
+        self.nextChar()
+
+    def nextChar(self):
+        self.curPos += 1
+        if self.curPos >= len(self.source):
+            self.curChar = '\0'
+        else:
+            self.curChar = self.source[self.curPos]
+
+    def peek(self):
+        if self.curPos + 1 >= len(self.source):
+            return '\0'
+        else:
+            return self.source[self.curPos+1]
+
+    def abort(self, message):
+        pass
+
+    def skipWhitespace(self):
+        pass
+
+    def skipComment(self):
+        pass
+
+    def getToken(self):
+        pass
